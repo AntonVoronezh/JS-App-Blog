c2d1a60c1158040e801013510b83037ff2e27898
AntonVoronezh <pstut@mail.ru>
2019-04-05 18:40:05 +0300

    create.component.js

---
+++ src/components/create.component.js
@@ -0,0 +1,47 @@
+import {
+    Component
+} from '../core/component'
+import {
+    Form
+} from '../core/form'
+import {
+    Validators
+} from '../core/validators'
+import {
+    apiService
+} from '../services/api.service'
+
+export class CreateComponent extends Component {
+    constructor(id) {
+        super(id);
+    }
+
+    init() {
+        this.$el.addEventListener('submit', submitHandler.bind(this));
+
+        this.form = new Form(this.$el, {
+            title: [Validators.requred],
+            fulltext: [Validators.requred, Validators.minLength(10)]
+        });
+    }
+
+}
+
+async function submitHandler(event) {
+    event.preventDefault();
+
+    if (this.form.isValid()) {
+        const formData = {
+            type: this.$el.type.value,
+            date: new Date().toLocaleDateString(),
+            ...this.form.value()
+        }
+
+        await apiService.createPost(formData);
+        this.form.clear();
+
+    }
+
+
+
+};
